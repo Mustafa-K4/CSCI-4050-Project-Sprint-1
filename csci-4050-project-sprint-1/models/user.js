9f4ecc9d2008b167d8f6d192de163f2afc65829a
Henry Tran <henry.tran20003@gmail.com>
2026-03-25 23:17:17 -0400

    register

--- csci-4050-project-sprint-1/models/user.js
+++ csci-4050-project-sprint-1/models/user.js
@@ -7,20 +7,21 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    pswrd: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
+      unique: true,
       trim: true,
       lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
     },
     address: {
       type: [mongoose.Schema.Types.Mixed],
